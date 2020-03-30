5467700cd25585f364d724de3521d29c49775bdf
gjacopo <gjacopo@gmail.com>
2020-03-30 10:22:44 +0200

    Update __init__.py
    updated initialisation file

--- src/pyhcs/__init__.py
+++ src/pyhcs/__init__.py
@@ -11,11 +11,13 @@ services
 # *credits*:      `gjacopo <gjacopo@ec.europa.eu>`_ 
 # *since*:        Sun Mar 29 16:21:29 2020
 
-from os import path as osp
+from os import path as __osp
 
 __basename          = 'hcs'
 
-__packages          = ['numpy', 'pandas', 'geopy', 'geojson', 'happygisco']
+__packages          = ['numpy', 'pandas', 'json', 
+                       'geopy', 'geojson', 'happygisco', 'pyproj', 
+                       'googletrans']
 
 __all__             = ['%s%s' % (a,__basename) for a in ['base' , 'all']]
 
@@ -31,25 +33,26 @@ __countries         =  {"EU27_2020":
                         "EU27_2009": 
                             ["BE", "BG", "CZ", "DK", "DE", "EE", "IE", "EL", "ES", "FR", "IT", "CY", "LV", "LT", "LU", "HU", "MT", "NL", "AT", "PL", "PT", "RO", "SI", "SK", "FI", "SE", "UK" 
                              ] 
-                        }
-    
+                        }    
+__territories       = {}
+
 __all_countries     = "EU28"
     
 COUNTRIES           = __countries[__all_countries]
 
-__thisdir = osp.dirname(__file__)
+__thisdir = __osp.dirname(__file__)
 
 for __cc in COUNTRIES:
     __basesrc = '%s%s' % (__cc, __basename) 
     __fsrc = '%s.py' % __basesrc 
     try:
-        if osp.exists(osp.join(__thisdir, __fsrc)):
+        if __osp.exists(__osp.join(__thisdir, __fsrc)):
             __all__.append(__basesrc)   
     except:
         pass
     __fmeta = '%smeta.json' % __cc
     try:
-        if osp.exists(osp.join(__thisdir, __fmeta)):
+        if __osp.exists(__osp.join(__thisdir, __fmeta)):
             pass
     except:
         pass
