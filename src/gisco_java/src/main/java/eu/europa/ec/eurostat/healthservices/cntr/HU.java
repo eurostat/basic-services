dd76e9149482fecce56479ac0df579d8b6cf0633
jgaffuri <jgaf@protonmail.com>
2020-04-16 21:00:26 +0200

    HU format

--- src/gisco_java/src/main/java/eu/europa/ec/eurostat/healthservices/cntr/HU.java
+++ src/gisco_java/src/main/java/eu/europa/ec/eurostat/healthservices/cntr/HU.java
@@ -1,8 +1,8 @@
 package eu.europa.ec.eurostat.healthservices.cntr;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.commons.csv.CSVFormat;
 
@@ -16,71 +16,97 @@ public class HU {
 		System.out.println("Start");
 
 		String cc = "HU";
-		
-		try {
-			//load data
-			CSVFormat cf = CSVFormat.DEFAULT.withDelimiter('\t').withFirstRecordAsHeader();
-			Collection<Map<String, String>> data = CSVUtil.load(HCUtil.path + "HU/NEAK_Fin_2020.01_02.csv", cf);
-			System.out.println(data.size());
 
+		//load data
+		CSVFormat cf = CSVFormat.DEFAULT.withDelimiter('\t').withFirstRecordAsHeader();
+		List<Map<String, String>> data = CSVUtil.load(HCUtil.path + "HU/NEAK_Fin_2020.01_02.csv", cf);
+		System.out.println(data.size());
 
-			//todo filter
-			Collection<Map<String, String>> data_ = new ArrayList<>();
-			for(Map<String, String> d : data) {
-				if(d.get("Aktív fekvőbeteg-szakellátás").isEmpty()
-						&& d.get("Fekvőbeteg-szakellátás").isEmpty()
-						&& d.get("Járó és - vagy fekvőbeteg-szakellátás").isEmpty()
-						/*&& d.get("Járóbeteg-szakellátás").isEmpty()*/)
-					continue;
-				data_.add(d);
-			}
-			data = data_; data_ = null;
-			System.out.println(data.size());
+		//filter
+		data = data.stream().filter(d -> !d.get("Aktív fekvőbeteg-szakellátás").isEmpty()).collect(Collectors.toList());
+		System.out.println(data.size());
 
-			CSVUtil.removeColumn(data, "Megyekód");
-			CSVUtil.removeColumn(data, "Megye név"); //TODO keep it for address?
+		//remove columns
+		for(String col : new String[]{
+				"Megyekód",
+				"Megye név", //county name. keep it for address?
+				"Pénzügyi típus kódja - megnevezése (2017.01.01-től)", "a", "Tulajdonos", "Vezető", "Beosztás",
+				"Háziorvosi szolgálat",
+				"Háziorvosi ügyelet",
+				"Iskola egészségügyi ellátás",
+				"Védőnői ellátás",
+				"Anya-gyermek- és csecsemővédelem",
+				"Mozgó szakorvosi szolgálat",
+				"Fogászati ellátás",
+				"Gondozás",
+				"Otthoni szakápolás",
+				"Betegszállítás",
+				"Halottszálítás",
+				"Művesekezelés",
+				"Működési költségelőleg",
+				"Célelőirányzat",
+				"Mentés",
+				"Laboratóriumi ellátás",
+				"Járóbeteg-szakellátás",
+				"CT",
+				"Aktív fekvőbeteg-szakellátás",
+				"Krónikus fekvőbeteg-szakellátás",
+				"IM - BVOP",
+				"Várólista csökk. és szakmapol. célok",
+				"Extra finanszírozás",
+				"Nagyértékű gyógyszerfin.",
+				"Speciális finanszírozás",
+				"Összesen", //this is the total bidget? Indicator for capacity?
 
-			CSVUtil.removeColumn(data, "Csak CT");
-			CSVUtil.removeColumn(data, "Csak Labor");
-			CSVUtil.removeColumn(data, "Csak Járó");
-			CSVUtil.removeColumn(data, "Csak Aktív");
-			CSVUtil.removeColumn(data, "Csak Krónikus");
+				//Active inpatient specialist care
+				"Aktív fekvőbeteg-szakellátás",
+				//Inpatient care
+				"Fekvőbeteg-szakellátás",
+				//Outpatient and - or inpatient specialist care
+				"Járó és - vagy fekvőbeteg-szakellátás",
+				//Outpatient care
+				"Járóbeteg-szakellátás",
+				//CT only
+				"Csak CT",
+				//Labor only
+				"Csak Labor",
+				//Just Walking
+				"Csak Járó",
+				//Active only
+				"Csak Aktív",
+				//Only Chronic
+				"Csak Krónikus",
+		})
+			CSVUtil.removeColumn(data, col);
 
-			//TODO keep that for facility type?
-			CSVUtil.removeColumn(data, "Aktív fekvőbeteg-szakellátás");
-			CSVUtil.removeColumn(data, "Fekvőbeteg-szakellátás");
-			CSVUtil.removeColumn(data, "Járó és - vagy fekvőbeteg-szakellátás");
-			CSVUtil.removeColumn(data, "Járóbeteg-szakellátás");
+		CSVUtil.renameColumn(data, "Int.kód", "id");
+		CSVUtil.renameColumn(data, "Szolgáltató", "hospital_name");
+		CSVUtil.renameColumn(data, "Irányítószám", "postcode");
+		CSVUtil.renameColumn(data, "Település", "city");
+		CSVUtil.renameColumn(data, "Utca", "street");
+		//CSVUtil.renameColumn(data, "", "");
 
-			CSVUtil.renameColumn(data, "Int.kód", "id");
-			CSVUtil.renameColumn(data, "Irányítószám", "postcode");
-			CSVUtil.renameColumn(data, "Település", "city");
-			CSVUtil.renameColumn(data, "Utca", "street");
-			//CSVUtil.renameColumn(data, "", "");
+		//tel
+		for(Map<String, String> d : data) {
+			//tel
+			d.put("tel", d.get("Tel.körzet") + "-" + d.get("Telefonszám"));
 
-			for(Map<String, String> d : data) {
-				//tel
-				d.put("tel", d.get("Tel.körzet") + "-" + d.get("Telefonszám"));
-				
-				//System.out.println(d.get("Szolgáltató"));
-			}
-			CSVUtil.removeColumn(data, "Telefonszám");
-			CSVUtil.removeColumn(data, "Tel.körzet");
-
-			CSVUtil.addColumn(data, "cc", cc);
-			CSVUtil.addColumn(data, "ref_date", "01/02/2020");
-
-			CSVUtil.addColumn(data, "emergency", "");
-			CSVUtil.addColumn(data, "public_private", "");
-			CSVUtil.addColumn(data, "geo_qual", "3");
-			CSVUtil.addColumn(data, "lat", "0");
-			CSVUtil.addColumn(data, "lon", "0");
-
-			Validation.validate(data, "HU");
-
-		} catch (Exception e) {
-			e.printStackTrace();
+			//System.out.println(d.get("hospital_name"));
 		}
+		CSVUtil.removeColumn(data, "Telefonszám");
+		CSVUtil.removeColumn(data, "Tel.körzet");
+
+		CSVUtil.addColumn(data, "cc", cc);
+		CSVUtil.addColumn(data, "ref_date", "01/02/2020");
+
+		CSVUtil.addColumn(data, "site_name", "");
+		CSVUtil.addColumn(data, "emergency", "");
+		CSVUtil.addColumn(data, "public_private", "");
+		CSVUtil.addColumn(data, "geo_qual", "3");
+		CSVUtil.addColumn(data, "lat", "0");
+		CSVUtil.addColumn(data, "lon", "0");
+
+		Validation.validate(data, "HU");
 
 		System.out.println("End");
 	}
