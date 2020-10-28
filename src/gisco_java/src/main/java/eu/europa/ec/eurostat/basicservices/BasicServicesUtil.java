f22b284f37c04809c086a8b389638f18f9f1204b
jgaffuri <jgaf@protonmail.com>
2020-10-28 11:10:19 +0100

    restructure

---
+++ src/gisco_java/src/main/java/eu/europa/ec/eurostat/basicservices/BasicServicesUtil.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package eu.europa.ec.eurostat.basicservices;
+
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+
+import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
+
+/**
+ * @author gaffuju
+ *
+ */
+public class BasicServicesUtil {
+
+	//path to the shared-data folder
+	public static String path = "E:/dissemination/shared-data/MS_data/";
+
+	//date format
+	public static SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+}
