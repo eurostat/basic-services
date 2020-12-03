67e8f4e8b67c071f0b33de9243aa77838dd504a3
jgaffuri <jgaf@protonmail.com>
2020-12-03 20:14:44 +0100

    FR

--- src/gisco_java/src/main/java/eu/europa/ec/eurostat/basicservices/education/cntr/FR.java
+++ src/gisco_java/src/main/java/eu/europa/ec/eurostat/basicservices/education/cntr/FR.java
@@ -1,88 +1,106 @@
 package eu.europa.ec.eurostat.basicservices.education.cntr;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
+import java.io.File;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.commons.csv.CSVFormat;
 
-import eu.europa.ec.eurostat.basicservices.ServicesGeocoding;
 import eu.europa.ec.eurostat.basicservices.education.EducationUtil;
-import eu.europa.ec.eurostat.basicservices.education.Validation;
-import eu.europa.ec.eurostat.jgiscotools.geocoding.BingGeocoder;
 import eu.europa.ec.eurostat.jgiscotools.gisco_processes.LocalParameters;
 import eu.europa.ec.eurostat.jgiscotools.io.CSVUtil;
-import eu.europa.ec.eurostat.jgiscotools.io.geo.CRSUtil;
-import eu.europa.ec.eurostat.jgiscotools.io.geo.GeoData;
+import eu.europa.ec.eurostat.jgiscotools.io.web.HTTPUtil;
 
 public class FR {
 
 	public static void main(String[] args) {
 		System.out.println("Start");
+		/*
+		#https://www.education.gouv.fr/
+			#https://data.education.gouv.fr/
+			#https://data.education.gouv.fr/map/
+
+			# Annuaire de l'éducation
+			# Annuaire de l'éducation : données sur les établissements publics et privés ouverts situés en France. Le jeu de données couvre le premier degré, le second degré, les Centre d'Information et d'Orientation ainsi que les établissements administratifs.
+			# fr-en-annuaire-education
+			# 66460
+
+			#https://data.education.gouv.fr/explore/dataset/fr-en-annuaire-education/table/?disjunctive.nom_etablissement&disjunctive.type_etablissement&disjunctive.appartenance_education_prioritaire&disjunctive.type_contrat_prive&disjunctive.code_type_contrat_prive&disjunctive.pial
+			#https://data.education.gouv.fr/explore/dataset/fr-en-annuaire-education/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B
+		 */
+
+		//download data when FR_raw.csv file is not present
+		String url = "https://data.education.gouv.fr/explore/dataset/fr-en-annuaire-education/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B";
+		String inFilePath = EducationUtil.path + "FR/FR_raw.csv";
+		if(!new File(inFilePath).exists()) {
+			System.out.println("Download...");
+			LocalParameters.loadProxySettings();
+			HTTPUtil.downloadFromURL(url, inFilePath);
+		}
 
 		// load data
 		CSVFormat csvF = CSVFormat.DEFAULT.withFirstRecordAsHeader().withDelimiter(';');
-		ArrayList<Map<String, String>> data = CSVUtil.load(EducationUtil.path + "EE/Estonia_Education_Institutions.csv",
-				csvF);
-
+		List<Map<String, String>> data = CSVUtil.load(inFilePath, csvF);
 		System.out.println(data.size());
 
-		Collection<Map<String, String>> schoolsFormatted = new ArrayList<Map<String, String>>();
-		for (Map<String, String> s : data) {
+		//Identifiant_de_l_etablissement
+		//
+		//Adresse_1;Adresse_2;Adresse_3;Code postal;Code_commune;Nom_commune
+		//Code_departement;Code_academie;Code_region
+		//;Ecole_maternelle;Ecole_elementaire
+		//Voie_generale;Voie_technologique;Voie_professionnelle;
+		//Telephone;Fax;Web;Mail;
+		//Restauration;Hebergement;ULIS;Apprentissage;Segpa;
+		//Section_arts;Section_cinema;Section_theatre;Section_sport;Section_internationale;Section_europeenne;
+		//Lycee_Agricole;Lycee_militaire;Lycee_des_metiers;
+		//Post_BAC;Appartenance_Education_Prioritaire;GRETA;SIREN_SIRET
+		//;Nombre_d_eleves;
+		//Fiche_onisep;position
+		//;Type_contrat_prive;Libelle_departement;Libelle_academie;Libelle_region;
+		//coordonnee_X;coordonnee_Y;epsg;nom_circonscription;
+		//latitude;longitude;precision_localisation;
+		//date_ouverture;
+		//date_maj_ligne
+		//;etat;ministere_tutelle;etablissement_multi_lignes;
+		//"rpi_concentre";"rpi_disperse"
+		//;"code_nature";"libelle_nature;
+		
 
-			// new formatted school
-			HashMap<String, String> sf = new HashMap<String, String>();
 
-			// copy columns
-			sf.put("id", s.get("INSTITUTION_ID"));
-			sf.put("name", s.get("INSTITUTION_NAME"));
-			sf.put("levels", s.get("LEVEL OF EDUCATION"));
-			sf.put("facility_type", s.get("SCHOOL TYPE"));
-			sf.put("enrollment", s.get("ENROLLMENT"));
-			sf.put("email", s.get("EPOST"));
-			sf.put("url", s.get("URL"));
-			sf.put("postcode", s.get("POSTCODE"));
-			sf.put("city", s.get("SETTLEMENT"));
-			
+		//remove useless columns
+		CSVUtil.removeColumn(data, "Code_type_contrat_prive", "PIAL", "etablissement_mere", "type_rattachement_etablissement_mere", "code_bassin_formation", "libelle_bassin_formation");
+	
+		//filter Type_etablissement
+		//[Service Administratif, Ecole, Lycée, EREA, Collège, Information et orientation]
+		data = data.stream().filter(
+				d -> { return true; }
+				).collect(Collectors.toList());
+		System.out.println(data.size());
 
-			// address 
-			//Kooli tänav, 1
-			String ad = sf.get("ADDRESS");
-			
-			//if ad contains a comma, split it. else put address in street
-			if (ad.contains(",")) {
-				String[] parts = ad.split(",");
-				String houseNumber = parts[1];
-				sf.put("house_number", houseNumber);
-				String street = parts[0];
-				sf.put("street", street);
-					
-				if (parts.length != 2)
-					System.err.println(ad);
-			}
-			else {	
-				sf.put("street", s.get("ADDRESS"));
-			}
-			
-			
+		//rename columns
+		CSVUtil.renameColumn(data, "Nom_etablissement", "name");
 
-			// add to list
-			schoolsFormatted.add(sf);
+		//CSVUtil.getUniqueValues(data, "Statut_public_prive", true);
+		//[, Privé, Public, -]
 
-			// geocode
-			LocalParameters.loadProxySettings();
-			ServicesGeocoding.set(BingGeocoder.get(), schoolsFormatted, "lon", "lat", true, true);
 
-			Validation.validate(true, schoolsFormatted, "EE");
+		
+		//Collection<Map<String, String>> out = new ArrayList<Map<String, String>>();
 
-			// save
-			System.out.println(schoolsFormatted.size());
-			CSVUtil.save(schoolsFormatted, EducationUtil.path + "EE/EE.csv");
-			GeoData.save(CSVUtil.CSVToFeatures(schoolsFormatted, "lon", "lat"), EducationUtil.path + "EE/EE.gpkg",
-					CRSUtil.getWGS_84_CRS());
+		//for (Map<String, String> s : data) {
+		//}
 
-			System.out.println("End");
-		}
+		//Validation.validate(true, out, "FR");
+
+		//save
+		//System.out.println(out.size());
+		//CSVUtil.save(out, EducationUtil.path + "FR/FR.csv");
+		//GeoData.save(CSVUtil.CSVToFeatures(out, "lon", "lat"), EducationUtil.path + "FR/FR.gpkg", CRSUtil.getWGS_84_CRS());
+
+
+
+		System.out.println("End");
 	}
+
 }
