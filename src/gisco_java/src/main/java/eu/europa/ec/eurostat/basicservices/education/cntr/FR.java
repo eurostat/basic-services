96f017c48bbc85cca7bf374f808e95d1ca613665
jgaffuri <jgaf@protonmail.com>
2020-12-03 20:27:54 +0100

    FR

--- src/gisco_java/src/main/java/eu/europa/ec/eurostat/basicservices/education/cntr/FR.java
+++ src/gisco_java/src/main/java/eu/europa/ec/eurostat/basicservices/education/cntr/FR.java
@@ -44,18 +44,12 @@ public class FR {
 		List<Map<String, String>> data = CSVUtil.load(inFilePath, csvF);
 		System.out.println(data.size());
 
-		//Identifiant_de_l_etablissement
-		//
-		//Adresse_1;Adresse_2;Adresse_3;Code postal;Code_commune;Nom_commune
-		//Code_departement;Code_academie;Code_region
 		//;Ecole_maternelle;Ecole_elementaire
 		//Voie_generale;Voie_technologique;Voie_professionnelle;
-		//Telephone;Fax;Web;Mail;
 		//Restauration;Hebergement;ULIS;Apprentissage;Segpa;
 		//Section_arts;Section_cinema;Section_theatre;Section_sport;Section_internationale;Section_europeenne;
 		//Lycee_Agricole;Lycee_militaire;Lycee_des_metiers;
 		//Post_BAC;Appartenance_Education_Prioritaire;GRETA;SIREN_SIRET
-		//;Nombre_d_eleves;
 		//Fiche_onisep;position
 		//;Type_contrat_prive;Libelle_departement;Libelle_academie;Libelle_region;
 		//coordonnee_X;coordonnee_Y;epsg;nom_circonscription;
@@ -65,31 +59,54 @@ public class FR {
 		//;etat;ministere_tutelle;etablissement_multi_lignes;
 		//"rpi_concentre";"rpi_disperse"
 		//;"code_nature";"libelle_nature;
-		
+
+		//"site_name",
+		//"lat", "lon", "geo_qual",
+		//"levels", "max_students", "",
+		//"fields", "facility_type", "public_private",
+		//"ref_date"
+		//"comments"
+
 
 
 		//remove useless columns
-		CSVUtil.removeColumn(data, "Code_type_contrat_prive", "PIAL", "etablissement_mere", "type_rattachement_etablissement_mere", "code_bassin_formation", "libelle_bassin_formation");
-	
+		CSVUtil.removeColumn(data, "Fax", "Code_departement", "Code_academie", "Code_region", "Code_commune", "Code_type_contrat_prive", "PIAL", "etablissement_mere", "type_rattachement_etablissement_mere", "code_bassin_formation", "libelle_bassin_formation");
+
 		//filter Type_etablissement
 		//[Service Administratif, Ecole, Lycée, EREA, Collège, Information et orientation]
 		data = data.stream().filter(
-				d -> { return true; }
-				).collect(Collectors.toList());
+				d -> {
+					String t = d.get("Type_etablissement");
+					return t.equals("Ecole") || t.equals("Lycée") || t.equals("Collège");
+				}).collect(Collectors.toList());
 		System.out.println(data.size());
 
+		//TODO
+		//"street", "house_number" 
+		//Adresse_1;Adresse_2;Adresse_3
+
+
 		//rename columns
+		CSVUtil.renameColumn(data, "Identifiant_de_l_etablissement", "id");
 		CSVUtil.renameColumn(data, "Nom_etablissement", "name");
+		CSVUtil.renameColumn(data, "Code postal", "postcode");
+		CSVUtil.renameColumn(data, "Nom_commune", "city");
+		CSVUtil.renameColumn(data, "Nombre_d_eleves", "enrollment");
+		CSVUtil.renameColumn(data, "Telephone", "tel");
+		CSVUtil.renameColumn(data, "Web", "url");
+		CSVUtil.renameColumn(data, "Mail", "email");
+
+
+		//for (Map<String, String> s : data) {
+		//}
+		
+		//add columns
+		CSVUtil.addColumn(data, "cc", "FR");
+		CSVUtil.addColumn(data, "country", "France");
 
 		//CSVUtil.getUniqueValues(data, "Statut_public_prive", true);
 		//[, Privé, Public, -]
 
-
-		
-		//Collection<Map<String, String>> out = new ArrayList<Map<String, String>>();
-
-		//for (Map<String, String> s : data) {
-		//}
 
 		//Validation.validate(true, out, "FR");
 
