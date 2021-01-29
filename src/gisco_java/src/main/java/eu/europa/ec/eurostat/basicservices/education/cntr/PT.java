df43e852367b0bfb1989a65a69b0ba53b802f2de
kimberlycl <kimberlyrclemons@gmail.com>
2021-01-29 15:10:40 +0100

    PT

--- src/gisco_java/src/main/java/eu/europa/ec/eurostat/basicservices/education/cntr/PT.java
+++ src/gisco_java/src/main/java/eu/europa/ec/eurostat/basicservices/education/cntr/PT.java
@@ -7,6 +7,8 @@ import org.apache.commons.csv.CSVFormat;
 
 import eu.europa.ec.eurostat.basicservices.education.EducationUtil;
 import eu.europa.ec.eurostat.basicservices.education.Validation;
+import eu.europa.ec.eurostat.jgiscotools.geocoding.BingGeocoder;
+import eu.europa.ec.eurostat.jgiscotools.gisco_processes.LocalParameters;
 import eu.europa.ec.eurostat.jgiscotools.io.CSVUtil;
 import eu.europa.ec.eurostat.jgiscotools.io.geo.CRSUtil;
 import eu.europa.ec.eurostat.jgiscotools.io.geo.GeoData;
@@ -16,14 +18,23 @@ public class PT {
 	public static void main(String[] args) {
 		System.out.println("Start");
 		
-		String inFilePath = EducationUtil.path + "PT/PT.csv";
+		String inFilePath = EducationUtil.path + "PT/PT_formatted.csv";
 		
 		CSVFormat csvF = CSVFormat.DEFAULT.withFirstRecordAsHeader().withDelimiter(',');
 		List<Map<String, String>> data = CSVUtil.load(inFilePath, csvF);
 		System.out.println(data.size());
 
+		
+		// geocode
+		//set proxy
+		LocalParameters.loadProxySettings();
+		BingGeocoder.get();
+		//set bing geocoder key
+		BingGeocoder.key = LocalParameters.get("bing_map_api_key");
+
+		
 		//validation
-		Validation.validate(true, data, "PT");
+		//Validation.validate(true, data, "PT");
 
 		//save
 		System.out.println(data.size());
