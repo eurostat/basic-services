1ba41840673e11cf84a3f6cd1fbe3ec1878bb8ca
kimberlycl <kimberlyrclemons@gmail.com>
2021-02-01 10:11:01 +0100

    IE, PT draft

--- src/gisco_java/src/main/java/eu/europa/ec/eurostat/basicservices/education/cntr/IE.java
+++ src/gisco_java/src/main/java/eu/europa/ec/eurostat/basicservices/education/cntr/IE.java
@@ -7,7 +7,6 @@ import org.apache.commons.csv.CSVFormat;
 
 import eu.europa.ec.eurostat.basicservices.education.EducationUtil;
 import eu.europa.ec.eurostat.basicservices.education.Validation;
-import eu.europa.ec.eurostat.basicservices.healthcare.HealthcareUtil;
 import eu.europa.ec.eurostat.jgiscotools.io.CSVUtil;
 import eu.europa.ec.eurostat.jgiscotools.io.geo.CRSUtil;
 import eu.europa.ec.eurostat.jgiscotools.io.geo.GeoData;
@@ -22,15 +21,14 @@ public class IE {
 		ArrayList<Map<String, String>> data = CSVUtil.load(EducationUtil.path + "IE/IE_formatted.csv", csvF);
 
 		System.out.println(data.size());
-		
+
 		CSVUtil.addColumns(data, EducationUtil.cols, "");
 		CSVUtil.setValue(data, "cc", "IE");
 		CSVUtil.setValue(data, "geo_qual", "4");
 		CSVUtil.setValue(data, "ref_date", "30/6/2020");
 
-		//TODO why is this not removing the column?
+		// TODO why is this not removing the column?
 		CSVUtil.removeColumn(data, "Attribute");
-
 
 		Validation.validate(true, data, "IE");
 
